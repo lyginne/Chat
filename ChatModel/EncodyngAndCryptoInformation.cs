a966eb4910707d4323651c25600ca5f136c4bdb8
lyginne <lyginne@gmail.com>
2015-11-26 20:15:56 +0300

    Canges in message analyze

--- ChatModel/EncodyngAndCryptoInformation.cs
+++ ChatModel/EncodyngAndCryptoInformation.cs
@@ -5,9 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatModel {
-    static class EncodyngAndCryptoInformation {
+    public static class EncodyngAndCryptoInformation {
         public static Encoding encoding = Encoding.UTF8;
         public static System.Security.Cryptography.SHA256Managed hashingAlgorytm = new System.Security.Cryptography.SHA256Managed();
-        
     }
 }
